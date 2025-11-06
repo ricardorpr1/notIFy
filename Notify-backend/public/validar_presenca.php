3c59f4d4f55b013ddaebb804609426f5d3706fb5
Neargoldfish805 <berguiruleiton@gmail.com>
2025-11-06 18:00:28 -0300

    Validar presença funcionando

--- Notify-backend/public/validar_presenca.php
+++ Notify-backend/public/validar_presenca.php
@@ -41,7 +41,7 @@ $myRole = intval($_SESSION['role'] ?? 0);
 try {
     $pdo = new PDO("mysql:host={$DB_HOST};port={$DB_PORT};dbname={$DB_NAME};charset=utf8mb4", $DB_USER, $DB_PASS, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_EMULATE_PREPERES => false,
+        PDO::ATTR_EMULATE_PREPARES => false, // <-- CORREÇÃO AQUI
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 } catch (PDOException $e) {
